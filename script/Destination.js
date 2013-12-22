6832f7ed4bd49d5217d03a8ecc2b57d191788b93
strongpauly <strongpauly@hotmail.com>
2013-12-22 12:58:43 +0000

    Separate out Trips from Destinations

--- script/Destination.js
+++ script/Destination.js
@@ -1,116 +1,15 @@
 (function(){
 
 
-Destination = function(country, resort, flight, board, timeOffset, webcams, snowreports, maps, end, forecasts)
+Destination = function(country, resort, timeOffset, webcams, snowreports, maps, forecasts)
 {
 	this.country = country;
 	this.resort = resort;
-	this.flight = flight;
-	this.board = board;
 	this.offset = timeOffset;
 	this.webcams = webcams;
 	this.snowreports = snowreports;
 	this.maps = maps;
-	this.end = end;
 	this.forecasts = forecasts;
 }
 
-Destination.prototype.render = function(parent)
-{
-	this.dom = $(this.getHtml());
-	parent.append(this.dom);	
-}
-
-Destination.prototype.update = function(now)
-{
-	var localTime = now.addHours(this.offset);
-	$('.localTime', this.dom).html(localTime.toTimeString());
-	var end = this.end;
-	function getTimeHtml(date, offset, flight)
-	{
-		if(date < now)
-		{
-			var message;
-			var during = end > now;
-			if(flight)
-			{
-				message = during ? "You've arrived.  Get boarding!" : "It's over :( Whens the next trip?"; 
-			}
-			else			
-			{
-				message = during ? "You're there!  Hope you're having fun!" : "It's over :( Whens the next trip?"; 
-			}
-			return '<div class="pastEvent">'+message+'</div>'
-		}
-		return 	 '<div class="timeDays">' + now.timeUntilDays(date, offset) + '</div>'
-			+'<div class="timeHours">' + now.timeUntilHours(date, offset) + '</div>'
-			+'<div class="timeMinutes">' + now.timeUntilMinutes(date, offset) + '</div>'
-			+'<div class="timeSeconds">' + now.timeUntilSeconds(date, offset) + '</div>';
-	}
-	
-	$('.flightTime', this.dom).html(getTimeHtml(this.flight, 0, true));	
-	$('.boardTime', this.dom).html(getTimeHtml(this.board, this.offset, false));
-}
-
-Destination.prototype.getHtml = function()
-{
-	var html = ['<div class="'];
-	if(this.maps != null)
-	{
-		html.push('destination-map');
-	}
-	else
-	{
-		html.push('destination');
-	}
-	html.push('"><div class="times"><h2>');		
-	html.push((this.resort != null ? this.resort + ', ' + this.country : this.country) + ' (<span class="localTime"></span>)');	
-	html.push('</h2><h3>Flight: ' + this.flight.toDateTimeString() + '</h3>'
-		+'<div class="flightTime"></div>'
-		+'<h3>Snowboarding: '+this.board.toDateTimeString()+'</h3>'
-		+'<div class="boardTime"></div>');		
-	if(this.maps != null)
-	{
-		html.push('<h3>Maps</h3>');
-		html.push('<div class="maps">');
-		for (var i=0; i<this.maps.length; i++)
-		{
-			html.push(this.maps[i].getHtml());
-		}
-		html.push('</div>');
-	}
-	if(this.webcams != null)
-	{		
-		html.push('<h3>Web Cams</h3>');
-		html.push('<div class="webcams">');
-		for(var i=0; i<this.webcams.length; i++)
-		{
-			html.push(this.webcams[i].getHtml());
-		}
-		html.push('</div>');
-	}
-	if(this.forecasts != null)
-	{
-		html.push('<h3>Long range forecasts</h3>');
-		html.push('<div class="webcams">');
-		for(var i=0; i<this.forecasts.length; i++)
-		{
-			html.push(this.forecasts[i].getHtml());
-		}
-		html.push('</div>');
-	}
-
-	html.push('</div>');
-	if(this.snowreports != null && this.snowreports.length > 0)
-	{
-		html.push('<div class="snowreports">');
-		for (var i=0; i<this.snowreports.length; i++)
-		{
-			html.push('<span class="snowreport"><iframe src="'+this.snowreports[i]+'"></iframe></span>');
-		}
-		html.push('</div>');
-	}
-	html.push('</div>')
-	return html.join('');
-}
 })();
---
+++ script/Trip.js
@@ -0,0 +1,114 @@
+(function(){
+
+
+Trip = function(destination, title, flight, board, end)
+{
+	this.destination = destination;
+	this.title = title;
+	this.flight = flight;
+	this.board = board;
+	this.end = end;
+}
+
+Trip.prototype.render = function(parent)
+{
+	this.dom = $(this.getHtml());
+	parent.append(this.dom);	
+}
+
+Trip.prototype.update = function(now)
+{
+	var localTime = now.addHours(this.destination.offset);
+	$('.localTime', this.dom).html(localTime.toTimeString());
+	var end = this.end;
+	function getTimeHtml(date, offset, flight)
+	{
+		if(date < now)
+		{
+			var message;
+			var during = end > now;
+			if(flight)
+			{
+				message = during ? "You've arrived.  Get boarding!" : "It's over :( Whens the next trip?"; 
+			}
+			else			
+			{
+				message = during ? "You're there!  Hope you're having fun!" : "It's over :( Whens the next trip?"; 
+			}
+			return '<div class="pastEvent">'+message+'</div>'
+		}
+		return 	 '<div class="timeDays">' + now.timeUntilDays(date, offset) + '</div>'
+			+'<div class="timeHours">' + now.timeUntilHours(date, offset) + '</div>'
+			+'<div class="timeMinutes">' + now.timeUntilMinutes(date, offset) + '</div>'
+			+'<div class="timeSeconds">' + now.timeUntilSeconds(date, offset) + '</div>';
+	}
+	
+	$('.flightTime', this.dom).html(getTimeHtml(this.flight, 0, true));	
+	$('.boardTime', this.dom).html(getTimeHtml(this.board, this.destination.offset, false));
+}
+
+Trip.prototype.getHtml = function()
+{
+	var html = ['<div class="'];
+	var destination = this.destination;
+	if(destination.maps != null)
+	{
+		html.push('destination-map');
+	}
+	else
+	{
+		html.push('destination');
+	}
+	html.push('"><div class="times"><h2>');	
+	var title = this.title != null ? this.title : (destination.resort != null ? destination.resort + ', ' + destination.country : destination.country);
+	html.push(title);
+	html.push(' (<span class="localTime"></span>)');	
+	html.push('</h2><h3>Flight: ' + this.flight.toDateTimeString() + '</h3>'
+		+'<div class="flightTime"></div>'
+		+'<h3>Snowboarding: '+this.board.toDateTimeString()+'</h3>'
+		+'<div class="boardTime"></div>');		
+	if(destination.maps != null)
+	{
+		html.push('<h3>Maps</h3>');
+		html.push('<div class="maps">');
+		for (var i=0; i<destination.maps.length; i++)
+		{
+			html.push(destination.maps[i].getHtml());
+		}
+		html.push('</div>');
+	}
+	if(destination.webcams != null)
+	{		
+		html.push('<h3>Web Cams</h3>');
+		html.push('<div class="webcams">');
+		for(var i=0; i<destination.webcams.length; i++)
+		{
+			html.push(destination.webcams[i].getHtml());
+		}
+		html.push('</div>');
+	}
+	if(destination.forecasts != null)
+	{
+		html.push('<h3>Long range forecasts</h3>');
+		html.push('<div class="webcams">');
+		for(var i=0; i<destination.forecasts.length; i++)
+		{
+			html.push(destination.forecasts[i].getHtml());
+		}
+		html.push('</div>');
+	}
+
+	html.push('</div>');
+	if(destination.snowreports != null && destination.snowreports.length > 0)
+	{
+		html.push('<div class="snowreports">');
+		for (var i=0; i<destination.snowreports.length; i++)
+		{
+			html.push('<span class="snowreport"><iframe src="'+destination.snowreports[i]+'"></iframe></span>');
+		}
+		html.push('</div>');
+	}
+	html.push('</div>')
+	return html.join('');
+}
+})();
